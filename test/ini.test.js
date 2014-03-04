b261d83a7d0111397ab6be37189f08298679963a
Pat Cavit <pcavit@gmail.com>
2014-03-04 13:21:45 -0800

    Functioning line-by-line parser

--- test/ini.test.js
+++ test/ini.test.js
@@ -1,15 +1,33 @@
 /*jshint node:true */
 "use strict";
 
-var fs  = require("fs"),
-    ini = require("./lib/ini");
+var fs     = require("fs"),
+    assert = require("assert"),
+
+    ini    = require("../lib/ini");
 
 describe("arena-ini", function() {
     describe("parser", function() {
         it("should parse simple configs", function() {
             var o = ini(fs.readFileSync("./test/specimens/simple.ini", "utf8"));
             
-            console.log(o);
+            //console.log(JSON.stringify(o, null, 4));
+
+            assert(o["Fooga.Booga"]);
+            assert(o.woo);
+
+            assert.deepEqual(
+                o["Fooga.Booga"][0],
+                {
+                    name   : "Nooga",
+                    value  : "Looga, Wooga, Googa",
+                    values :  [
+                        "Looga",
+                        "Wooga",
+                        "Googa"
+                    ]
+                }
+            );
         });
     });
 });
